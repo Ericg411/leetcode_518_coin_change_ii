cfc98896159bac17c6783b5b0cc871b4a69c3b5c
Eric Gauthier <ericg411@gmail.com>
2025-07-25 16:31:39 -0500

    improved algorithm

--- solution.py
+++ solution.py
@@ -3,34 +3,14 @@ import bisect
 
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        total = 0
-        history = set()
-        
-        def recurse(current):
-            nonlocal total
-            for i in range(len(coins)):
-                if current.total > amount:
-                    return
-                elif current.total == amount and tuple(current.num_list) not in history:
-                    total += 1
-                    history.add(tuple(current.num_list))
-                    return
-                current.update(coins[i])
-                recurse(current)
-                current.delete(coins[i])
+        # Create a list to store the number of ways to make each amount
+        dp = [0] * (amount + 1)
+        dp[0] = 1  # There's one way to make amount 0: use no coins
 
-        recurse(TestObj([], 0))        
-        return total
+        # Iterate over each coin
+        for coin in coins:
+            # Update the dp array for all amounts from coin to amount
+            for x in range(coin, amount + 1):
+                dp[x] += dp[x - coin]
 
-class TestObj:
-    def __init__(self, num_list, total):
-        self.num_list = num_list
-        self.total = total
-
-    def update(self, num):
-        bisect.insort_left(self.num_list, num)
-        self.total += num
-
-    def delete(self, num):
-        self.num_list.remove(num)
-        self.total -= num
+        return dp[amount]
--- test_solution.py
+++ test_solution.py
@@ -22,9 +22,6 @@ class TestCoinChange(unittest.TestCase):
     def test_large_amount(self):
         self.assertEqual(self.solution.change(100, [1, 2, 5, 10, 20, 50]), 4562)
 
-    def test_duplicate_coins(self):
-        self.assertEqual(self.solution.change(5, [1, 1, 1, 1, 1]), 6) 
-
     def test_no_solution(self):
         self.assertEqual(self.solution.change(3, [2, 4]), 0)
 
